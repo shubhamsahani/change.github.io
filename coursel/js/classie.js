b73154429cba107b4a8717bdd9af39fc07f54854
Shubham SUnny <shubhamsahaninitkkr@gmail.com>
2018-03-13 16:41:50 +0530

    final code

--- coursel/js/classie.js
+++ coursel/js/classie.js
@@ -1,13 +1,28 @@
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
 
 ( function( window ) {
 
 'use strict';
 
+// class helper functions from bonzo https://github.com/ded/bonzo
 
 function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
 }
 
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
 if ( 'classList' in document.documentElement ) {
@@ -57,9 +72,6 @@ var classie = {
 if ( typeof define === 'function' && define.amd ) {
   // AMD
   define( classie );
-} else if ( typeof exports === 'object' ) {
-  // CommonJS
-  module.exports = classie;
 } else {
   // browser global
   window.classie = classie;
